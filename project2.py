23650cb46709d8b73fa6270486ee5bfee66d4dce
PhamHuyHoang123 <137593633+PhamHuyHoang123@users.noreply.github.com>
2023-06-30 09:39:47 +0700

    Update project2.py

--- project2.py
+++ project2.py
@@ -5,33 +5,64 @@ from dash.dependencies import Input, Output
 import pandas as pd
 import plotly.express as px
 
+
 #getting the dataset
 
-data1 = pd.read_csv('C:/Users/huyho/Documents/code/Copy-of-adidas.csv')
+pro = pd.read_csv('C:/Users/huyho/Documents/code/data/Employee-Attrition.csv')
 
 #dash app
 
 app = dash.Dash()
-
+server = app.server
 #layout
 app.layout = html.Div(children = [
-	html.H1(children = 'The correlation between operating profit and six distinct types of products sold'), dcc.Dropdown(id = 'geo-dropdown',
-		options =[{'label': i, 'value' : i}
-				for i in data1['Product'].unique()],
-		value = "Men's Street Footwear"),
-	dcc.Graph(id = 'price-graph')
-])
+    html.Div([
+        html.H1(children = 'Analysis of employee attrition',
+                style={'text-align': 'center', 'font-size': '36px', 'color': '#333333', 'margin': '10px'})
+    ], style={'background-color': '#f8f9fa', 'padding': '20px'}),
+    html.Div([
+        html.Div([
+            dcc.Checklist(
+                id = 'geo-checklist',
+                options = [{'label': i, 'value': i}
+                           for i in pro['Department'].unique()],
+                value = ["Sales"],
+                labelStyle={'display': 'block', 'margin': '10px'}
+            ),
+            dcc.Graph(id = 'price-graph'),
+            dcc.RangeSlider(
+                id='range-slider',
+                min=pro['YearsInCurrentRole'].min(),
+                max=pro['YearsInCurrentRole'].max(),
+                step=1,
+                value=[pro['YearsInCurrentRole'].min(), pro['YearsInCurrentRole'].max()],
+                marks={str(i): str(i) for i in range(pro['YearsInCurrentRole'].min(), pro['YearsInCurrentRole'].max()+1, 1)}
+            )
+        ], className='six columns', style={'border': '1px solid #ced4da', 'border-radius': '5px', 'margin': '10px'}),
+        html.Div([
+            dcc.Graph(id = 'bar-chart')
+        ], className='six columns', style={'border': '1px solid #ced4da', 'border-radius': '5px', 'margin': '10px'})
+    ], className='row')])
 
 @app.callback(
-	Output(component_id = 'price-graph',component_property ='figure'),
-	Input(component_id = 'geo-dropdown',component_property = 'value')
+    Output(component_id = 'price-graph', component_property ='figure'),
+    Input(component_id = 'geo-checklist', component_property = 'value'),
+    Input(component_id = 'range-slider', component_property = 'value')
 )
+def update_scatter(selected_departments, selected_range):
+    data = pro[(pro['Department'].isin(selected_departments)) & (pro['YearsInCurrentRole'] >= selected_range[0]) & (pro['YearsInCurrentRole'] <= selected_range[1])]
+    graph = px.scatter(data, x ='YearsInCurrentRole', y = 'YearsAtCompany', color='Attrition')
+    return graph
 
-def update_graph(selected_product):
-	a = data1[data1['Product'] == selected_product]
-	graph = px.scatter(a, x ='Units Sold' , y = 'Operating Profit' )
-	return graph 
+@app.callback(
+    Output(component_id = 'bar-chart', component_property ='figure'),
+    Input(component_id = 'geo-checklist', component_property = 'value')
+)
+def update_bar(selected_departments):
+    data = pro[pro['Department'].isin(selected_departments)]
+    bar = px.histogram(data, x ='MonthlyIncome', nbins=30)
+    return bar
 
 if __name__ == '__main__':
-	app.run_server(debug = True)
+    app.run_server(debug = True)
 
